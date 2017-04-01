47c20ca73f8d805248508a141e158a3918ffea2b
wslx520 <lix9nw@gmail.com>
2017-04-01 22:53:27 +0800

    更新webpack版本号

--- my-react-demo/server.js
+++ my-react-demo/server.js
@@ -9,6 +9,12 @@ new WebpackDevServer(webpack(config), {
   // contentBase 即服务器根目录。比如有引用非webpack打包产生的文件，则会以此目录为根目录进行相关路径的定位
   contentBase: './dist',
   hot: true,
+  proxy: { // proxy URLs to backend development server
+      '/api': 'http://localhost:3000'
+      // 比如有接口服务器监听的是另一个端口，则可以在这儿代理一个出口，然后请求这个代理地址，就没有跨域问题了
+      // 比如，代理前：ajax('http://localhost:3000/data')，代理后：ajax('/api/data')
+      // 虽然接口服务器也可以通过设置跨域头解决跨域访问，但代理了还是可以减少url长度
+  },
   historyApiFallback: true
 }).listen(3000, '0.0.0.0', function (err, result) {
   // 0.0.0.0 可以适配 localhost, 127.0.0.1, 172.16.1.* 等多种情况
